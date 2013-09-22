ddf0fc7b6f85a20ca10126d5c4e5de14ab90dbac
Paul Park <camo1018@yahoo.com>
2013-09-21 21:35:00 -0400

    Added sql queries for the complete (for now) set of tables as well as dynamic table generation

---
+++ sql/Macros.sql
@@ -0,0 +1,53 @@
+--Populate Ingredients in Recipe Table
+declare @id int
+declare ingredientCursor cursor
+	for select id from Ingredient
+open ingredientCursor
+fetch next from ingredientCursor
+	into @id
+	while @@FETCH_STATUS = 0
+	begin
+		declare @sql nvarchar(100)
+		set @sql = 'alter table Recipe add ingredient' + cast(@id as nvarchar(10)) + 'Count int default(0)'
+		exec sp_executesql @sql
+		fetch next from ingredientCursor into @id
+	end
+close ingredientCursor
+deallocate ingredientCursor
+go
+	
+--Populate nutrients in Recipe Table
+declare @id int
+declare nutrientCursor cursor
+	for select id from Nutrient
+open nutrientCursor
+fetch next from nutrientCursor
+	into @id
+	while @@FETCH_STATUS = 0
+	begin
+		declare @sql nvarchar(100)
+		set @sql = 'alter table Recipe add nutrient' + cast(@id as nvarchar(10)) + 'Count int default(0)'
+		exec sp_executesql @sql
+		fetch next from nutrientCursor into @id
+	end
+close nutrientCursor
+deallocate nutrientCursor
+go
+
+--Populate Ingredients in UserIngredient Table
+declare @id int
+declare ingredientCursor cursor
+	for select id from Ingredient
+open ingredientCursor
+fetch next from ingredientCursor
+	into @id
+	while @@FETCH_STATUS = 0
+	begin
+		declare @sql nvarchar(100)
+		set @sql = 'alter table UserRecipe add ingredient' + cast(@id as nvarchar(10)) + 'Count int default(0)'
+		exec sp_executesql @sql
+		fetch next from ingredientCursor into @id
+	end
+close ingredientCursor
+deallocate ingredientCursor
+go
--- sql/Tables.sql
+++ sql/Tables.sql
@@ -1,7 +1,13 @@
 create table Ingredient (
 id int primary key identity,
-ingredientType varchar(255) not null,
+ingredientName varchar(255) not null,
 measuringUnit varchar(255) not null
+)
+
+create table Nutrient (
+id int primary key identity,
+nutrientName varchar(255) not null,
+dailyValue float
 )
 
 create table Recipe (
@@ -9,18 +15,19 @@ id int primary key identity,
 recipeName varchar(255) not null,
 imageUrl varchar(MAX),
 healthRating int default(0),
-timeToComplete int default(0),
-ingredient1Id int foreign key references Ingredient(id),
-ingredient1Count float default(0),
-ingredient2Id int foreign key references Ingredient(id),
-ingredient2Count float default(0),
-ingredient3Id int foreign key references Ingredient(id),
-ingredient3Count float default(0),
-ingredient4Id int foreign key references Ingredient(id),
-ingredient4Count float default(0),
-ingredient5Id int foreign key references Ingredient(id),
-ingredient5Count float default(0),
+timeToComplete int default(0)
+-- Need to add all the ingredients and nutrients counts here as well.
+)
 
+create table UserRecipe (
+id int primary key identity,
+userId int not null foreign key references RegisteredUser(id),
+recipeId int not null foreign key references Recipe(id)
+)
+
+create table UserIngredient (
+id int primary key identity,
+-- Need to add all the ingredient counts here.
 )
 
 create table RegisteredUser (
